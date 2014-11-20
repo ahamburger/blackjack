ccbd9bb0d9a6cf309b0d13b4d2300dd1e6cbdf29
Allison Hamburger <aghamburger@gmail.com>
2014-11-19 22:51:07 -0500

    Lots of comments and code cleanup

--- deck.py
+++ deck.py
@@ -1,13 +1,19 @@
 import random
 
+#DECK class: keeps track of cards available and picking cards to deal
+
 class Deck:
-	def __init__(self, card_usage=[], cards_left=[]):
+	def __init__(self, cards_left=[]):
 		self.reset()
 
 	def reset(self):
-		self._card_usage = [1]*52
-		self._cards_left = range(0,52)
+		#self._card_usage = [1]*52			# each index represents a unique card. 1 at that index if available, 0 otherwise
+											# a helper to assure that we aren't using cards multiple times before a shuffle
 
+		self._cards_left = range(0,52)		# each value 0-51 correlates with a unique card. cards are removed from this list as they 
+											# are used.
+
+	# returns index (0-51) of randomly selected card from remaining deck
 	def pickCard(self):
 		if len(self._cards_left)==0:
 			print "out of cards"	#TODO reshuffle? will this happen?
@@ -16,11 +22,13 @@ class Deck:
 		randomCard = random.randint(0,len(self._cards_left)-1) # random number between 0 and len(cards_left)
 		cardIndex = self._cards_left.pop(randomCard)	
 
-		assert self._card_usage[cardIndex] == 1, "Card already used"
-		
-		self._card_usage[cardIndex] = 0
+		#Tests to assure not re-using cards before shuffle
+		#assert self._card_usage[cardIndex] == 1, "Card already used"		
+		#self._card_usage[cardIndex] = 0
+
 		return cardIndex
 
+	#returns string representing the value of the card ie "a 2" or "an 8" or "a Jack"
 	def cardify(self, indexNum):
 		number = (indexNum % 13) + 1
 		if number == 1:
@@ -37,17 +45,19 @@ class Deck:
 		if number == 13:
 			return "a King"
 
+	#returns true if there are fewer than 5 cards left in the deck 
 	def tooFewCards(self):
 		return len(self._cards_left) < 5
 
 
-	#TODO explain ace logic
+	#returns the sum of the cards (represented as their card indices, 0-51) in cardList
+	#evaluates Aces so that use as 11 if that sum is closer to 21 (but not over)
 	def addCards(self, cardList):
 		sum = 0
-		haveAce = False
+		haveAce = False		#used to keep track of if there is an ace in the hand
 		
 		for c in cardList:
-			number = (c % 13) + 1
+			number = (c % 13) + 1 	#convert to numerical value of card instead of card index
 			if number > 10:		# treat jack, queen, king as 10
 				number = 10	
 			
@@ -56,14 +66,19 @@ class Deck:
 
 			sum += number
 				
+		# if there's one or more aces, check to see if counting one ace as an 11 will be beneficial
 		if haveAce and (sum + 10 <=21):
 			sum += 10		
 
 		return sum
 
+
+	#returns true if the cardList contains exactly one 6 and one Ace
 	def isSoftSeventeen(self,cardList):
-		if not len(cardList) == 2:
+		if not len(cardList) == 2:		#short circuit if more than 2 cards
 			return False
+
+		#convert to numerical value of card instead of card index
 		card1 = (cardList[0]%13) + 1
 		card2 = (cardList[1]%13) + 1
 		
@@ -71,6 +86,8 @@ class Deck:
 			return True
 		return False
 
+	#Return a string that is a sentence describing the cards in cardList. If faceDown is true, then conceal the first
+	#card in the list
 	def stringify(self, cardList, faceDown):
 		cards = ""
 		comma = ", "
@@ -80,10 +97,11 @@ class Deck:
 		for c in cardList:
 			if c == cardList[0] and faceDown:
 				continue			
-			elif c == cardList[-1] and not faceDown:	#last card
+			elif c == cardList[-1] and not faceDown:	#last card. If face down then we don't want the "and" 
 				cards += "and " + self.cardify(c) + "."
 			else:
 				cards += self.cardify(c)+ comma
+
 		if faceDown:
 			cards += "and a face down card."
 		return cards
--- game.py
+++ game.py
@@ -1,115 +1,131 @@
 from deck import Deck
 import sys
 
+### GAME class: controls game play, from dealing to picking a winner. Uses a Deck to keep track of cards
+
+
 class Game:
 	def __init__(self):
 		self._deck = Deck()
-		self._pCards = []	#player cards
-		self._dCards = []	#dealer cards
-		self._round = 1
-		self._wins = 0.0
-		self._total_rounds = 1.0
+		self._pCards = []			#player cards
+		self._dCards = []			#dealer cards
+		self._round = 1  			#number of rounds (1-6)
+		self._wins = 0.0			#total number of wins
+		self._total_rounds = 1.0	#total number of rounds (except for ties)
 
+
+	# Beginning of a new round-- deal 2 cards each to player and dealer
 	def deal(self):
-		#dealer gets 2 cards (both face up?)	TODO make one face down?
+		#dealer gets 2 cards
 		self._dCards = [self._deck.pickCard(), self._deck.pickCard()]
 
-		#player gets 2 cards (one face up, one face down)
+		#player gets 2 cards
 		self._pCards = [self._deck.pickCard(), self._deck.pickCard()]
+		
+		#Tell player their cards and the "face-up" dealer card
 		print("\n*******\nThe dealer has been dealt " + self._deck.stringify(self._dCards, True))
 		print("You've been dealt " + self._deck.stringify(self._pCards, False))
 
+
+	# Add one card from the deck to the parameter pile
 	def dealOne(self, card_pile):
 		card = self._deck.pickCard()
 		card_pile.append(card)
 
-
-	#return cards that player has 
-	#if busted, removes those cards from deck, start new game
+ 
+	#Returns True if player busted, False otherwise
+	#Prompts user to hit or stay. If hit, then asks again. If stay, exits the function. 
 	def playerTurn(self):
-		#hit or stay or bust
-		move = self.clean_raw_input("\nWould you like to hit (h) or stay (s)? \n")
+		move = self.clean_raw_input("\nWould you like to hit (h) or stay (s)? \n(Enter q to quit game.)\n")
 		if move == 'h':
-			self.dealOne(self._pCards)
+			#HIT
+			self.dealOne(self._pCards)		#give the player another card
 
+			#BUST
 			if self._deck.addCards(self._pCards) > 21:
 				print("\n*******\nNow you have "  + self._deck.stringify(self._pCards, False))
 				print ("Oops, you busted!\n")
 				return True
-
-			print("\n*******\nNow you have "  + self._deck.stringify(self._pCards, False))		
+		
+			#prompt again for hit or stay	
+			print("\n*******\nNow you have "  + self._deck.stringify(self._pCards, False))	
 			return self.playerTurn()
 		elif move == 'q':
+			#exit program
 			return sys.exit("Quitting")
-
 		elif move != 's':
 			print("Invalid selection. Try again. \n")
 			return self.playerTurn()
 
+		#STAY
 		return False
 
-	#return cards that dealer has
-	#if busted, removes those cards from deck, start new game
+	#Returns True if dealer busted, False otherwise
 	def dealerTurn(self):
-		#hit or stay or bust
+		#sum of current cards
 		sum = self._deck.addCards(self._dCards)
 		
 		if sum < 17 or self._deck.isSoftSeventeen(self._dCards):
+			#HIT
 			self.dealOne(self._dCards)
+
+			#BUST
 			if self._deck.addCards(self._dCards) > 21:
 				print("\nThe dealer hits, and now has " + self._deck.stringify(self._dCards, True))
 				print("The dealer busted! The dealer's face down card was " + self._deck.cardify(self._dCards[0]) + ".")
 				self.clean_raw_input("\nPress ENTER to continue.")
 				return True
+
 			print("\nThe dealer hits, and now has " + self._deck.stringify(self._dCards, True))	
 			self.clean_raw_input("\nPress ENTER to continue.")
 			return self.dealerTurn()
-		else:
-			self.clean_raw_input("\nThe dealer stays. \n\nPress ENTER to continue.");
 
-		
+		#STAY
+		self.clean_raw_input("\nThe dealer stays. \n\nPress ENTER to continue.");
 		return False
 
+
+	#Determine whether the player wins, the dealer wins, or if there is a tie. Only called when no one busts.
+	#Returns string to signify win, loss, tie
 	def pickWinner(self):
+		#calculate sums of cards
 		d_sum = self._deck.addCards(self._dCards)
 		p_sum = self._deck.addCards(self._pCards)
 		print("\n*******\nYour cards sum to " + str(p_sum) + ", and the dealer's cards sum to " + str(d_sum) + ".")
 
 		if (21-p_sum < 21-d_sum):	#player is closer to 21 than dealer
-			self._wins += 1.0
-			return "WIN!"
+			return "w"
 		elif (21-p_sum == 21-d_sum): #tie
-			self.total_rounds-=1   #don't want to affect our win percentage
-			return "and the dealer TIE."
+			return "t"
 		else:
-			return "LOSE :("
+			return "l"
 
 	
-	# deal cards, then play turns
+	# Deal cards, then play turns, reveal winner, ask to play again
 	def play(self):
 		self.deal()
 
-		pBust = self.playerTurn()
+		pBust = self.playerTurn()	#True if player busted (lost, and don't want to have dealer's turn)
+		win = "l"
 
-		win = "LOSE :("
 		if not pBust:
 			print("\n*******\nThe dealer has "  + self._deck.stringify(self._dCards, True))
-			dBust = self.dealerTurn()
-
+			dBust = self.dealerTurn() #True if dealer busted (player won)
+			
 			if dBust:
-				win = "WIN!"
-				self._wins += 1
+				win = "w"
 			else:
-				win = self.pickWinner() #True if player won, false if lost
+				win = self.pickWinner() # calculates winner based on sum of card piles
 
 		#ask to play again
 		if (self.playAgain(win)):
-			self.reset()
+			#start a new round
+			self.newRound()
 			self.play()
 
 
 	#update round numbers, reset player/dealer cards to none
-	def reset(self):
+	def newRound(self):
 		if (self._round == 6 or self._deck.tooFewCards()):
 			self._round = 1
 			self._deck = Deck()
@@ -124,14 +140,38 @@ class Game:
 
 
 	#Prompt with win percentage, then ask if want to play again
+	#Returns true if opt to play again, false with any other input (ends game)
 	def playAgain(self,win):
-		msg = "You " + win + "\n"
+		status = "LOSE :("
+		if win == 'w':
+			status = "WIN!"
+			self._wins += 1
+		elif win == 't':
+			self.total_rounds-=1   #don't want to affect our win percentage
+			status = "tied with the dealer."
 
-		msg += "\nYou've won " + str(int(self._wins)) + " times out of " + str(int(self._total_rounds)) + " rounds.\n" #TODO adjust plurality
+
+		#Printing of win percentage
+		msg = "You " + status + "\n\nYou've won " + str(int(self._wins))	
+		#Plurality checking-- "one time" instead of "one times", etc
+		if int(self._wins) == 1:
+			msg += " time"
+		else:
+			msg += " times"
+		msg += " out of " + str(int(self._total_rounds))
+
+		if  int(self._total_rounds) == 1:
+			msg += " round.\n"
+		else:
+			msg+= " rounds.\n" 
+
 		msg += "That makes your total win percentage: " + str(int(100.0 * self._wins/self._total_rounds)) + "%. \n"
+
+		#ask to play again
 		again = self.clean_raw_input(msg + "\nWould you like to play again? (y or n) \n")
 		return again == 'y'
 
+	# Helper function so ctrl-D and ctrl-C won't throw errors
 	def clean_raw_input(self, string):
 		try:
 			return raw_input(string)
